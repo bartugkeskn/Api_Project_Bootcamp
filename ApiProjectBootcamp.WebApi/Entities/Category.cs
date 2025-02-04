7c8935a0aefd0ded9beb76997fdcb3625e0733cd
bartugkeskn <bartukeskin1999@gmail.com>
2025-02-04 13:58:06 +0300

    Add project files.

---
+++ ApiProjectBootcamp.WebApi/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiProjectBootcamp.WebApi.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
