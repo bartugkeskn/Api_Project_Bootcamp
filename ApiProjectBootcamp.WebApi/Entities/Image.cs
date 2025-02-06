1de1c2dd56589dbc8dbc1f6e925d665425e08876
bartugkeskn <bartukeskin1999@gmail.com>
2025-02-06 13:15:32 +0300

    ...

---
+++ ApiProjectBootcamp.WebApi/Entities/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProjectBootcamp.WebApi.Entities
+{
+    public class Image
+    {
+        public int ImageID { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
