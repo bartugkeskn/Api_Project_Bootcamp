32d5c1d797b6d798a7b407b4a15165acbe7d5912
bartugkeskn <bartukeskin1999@gmail.com>
2025-02-27 15:10:26 +0300

    ...

---
+++ ApiProjectBootcamp.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using ApiProjectBootcamp.WebApi.Dtos.FeatureDtos;
+using ApiProjectBootcamp.WebApi.Dtos.MessageDtos;
+using ApiProjectBootcamp.WebApi.Entities;
+using AutoMapper;
+
+namespace ApiProjectBootcamp.WebApi.Mapping
+{
+    public class GeneralMapping : Profile  // Miras alınan Profile class'ı AutoMapper paketinden geliyor.
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature, ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature, CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature, UpdateFeatureDto>().ReverseMap();
+            CreateMap<Feature, GetByIdFeatureDto>().ReverseMap();
+
+            CreateMap<Message, ResultMessageDto>().ReverseMap();
+            CreateMap<Feature, CreateMessageDto>().ReverseMap();
+            CreateMap<Feature, UpdateMessageDto>().ReverseMap();
+            CreateMap<Feature, GetByIdMessageDto>().ReverseMap();
+        }
+    }
+}
